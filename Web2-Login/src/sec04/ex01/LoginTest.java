4c3fe4c7c2411c3311cc4e1879f8ff6999c774db
목재민 <mokjaemin@mogjaemin-ui-MacBookPro.local>
2023-01-11 13:57:28 +0900

    Try to connect DB

--- Web2-Login/src/sec04/ex01/LoginTest.java
+++ Web2-Login/src/sec04/ex01/LoginTest.java
@@ -46,19 +46,31 @@ public class LoginTest extends HttpServlet {
 		System.out.println("아이디 : " + id);
 		System.out.println("비밀번호 : " + pw);
 		
+		
+		// 아이디 정상 입력
 		if(id != null && (id.length() != 0)) {
-			out.print("<html>");
-			out.print("<body>");
-			out.print(id + "님 로그인하셨습니다.");
-			out.print("</body>");
-			out.print("</html>");
+			if(id.equals("admin")) {
+				out.print("<html><body>");
+				out.print("<font size = '12'>관리자로 로그인하였습니다.</font>");
+				out.print("<br>");
+				out.print("<input type='button' value='회원정보 수정하기'>");
+				out.print("<input type='button' value='회원정보 삭제하기'>");
+				out.print("<body><html>");
+			}
+			else {
+				out.print("<html>");
+				out.print("<body>");
+				out.print(id + "님 로그인하셨습니다.");
+				out.print("</body>");
+				out.print("</html>");
+			}
 		}
 		else {
 			out.print("<html>");
 			out.print("<body>");
 			out.print("아이디를 입력하세요!!");
 			out.print("<br>");
-			out.print("<a href = 'http://localhost:8080/Wev2-Login/test01/login.html'>로그인창으로 이동</a>");
+			out.print("<a href = 'http://localhost:8080/Web2-Login/test01/login3.html'>로그인창으로 이동</a>");
 			out.print("</body>");
 			out.print("</html>");
 		}
