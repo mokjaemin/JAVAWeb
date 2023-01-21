d138a35ea3ad8ca5854b6bb4a7fc86ab63de395b
목재민 <mokjaemin@mogjaemin-ui-MacBookPro.local>
2023-01-21 17:29:04 +0900

    Binding

---
+++ Binding/src/sec04/ex01/ContextFileServlet.java
@@ -0,0 +1,57 @@
+package sec04.ex01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextFileServlet
+ */
+@WebServlet("/cfile")
+public class ContextFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext();
+		
+		InputStream is = context.getResourceAsStream("/WEB-INF/bin/init.txt");
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+		
+		String menu = null;
+		String menu_member = null;
+		String menu_order = null;
+		String menu_goods = null;
+		
+		while((menu = buffer.readLine()) != null) {
+			StringTokenizer tokens = new StringTokenizer(menu, ",");
+			menu_member = tokens.nextToken();
+			menu_order = tokens.nextToken();
+			menu_goods = tokens.nextToken();
+		}
+		
+		out.print("<html><body>");
+		out.print("<table border='1' cellspacing='0'><tr>메뉴이름</tr>");
+		out.print("<tr><td>" + menu_member + "</tr></td>");
+		out.print("<tr><td>" + menu_order + "</tr></td>");
+		out.print("<tr><td>" + menu_goods + "</tr></td>");
+		out.print("</table></body></html>");
+			
+	}
+
+}
---
+++ Binding/src/sec05/ex01/InitParamServlet.java
@@ -0,0 +1,47 @@
+package sec05.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InitParamServlet
+ */
+@WebServlet(
+		urlPatterns = { 
+				"/sinit", 
+				"/sinit2"
+		}, 
+		initParams = { 
+				@WebInitParam(name = "email", value = "bamer@naver.com"), 
+				@WebInitParam(name = "tel", value = "010-5370-1670")
+		})
+public class InitParamServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String email = getInitParameter("email");
+		String tel = getInitParameter("tel");
+		
+		out.print("<html><body>");
+		out.print("<table border='1' cellspacing='0'><tr>메뉴이름</tr>");
+		out.print("<tr><td>" + email + "</tr></td>");
+		out.print("<tr><td>" + tel + "</tr></td>");
+		out.print("</table></body></html>");
+		
+	}
+
+}
